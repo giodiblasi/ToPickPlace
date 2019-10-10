2fb7d88dbb71e590deb63abcf5e0c8c63d6a9864
Giovanni Di Blasi <gio.diblasi@gmail.com>
2019-10-10 20:33:59 +0200

    add reduxDevTools

--- ToPickPlaceUI/pages/_app.tsx
+++ ToPickPlaceUI/pages/_app.tsx
@@ -3,10 +3,11 @@ import App from 'next/app'
 import {rootReducer} from '../store/index'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,composeWithDevTools());
+
 class MyApp extends App {
-
   render() {
     const { Component, pageProps } = this.props
     return (
