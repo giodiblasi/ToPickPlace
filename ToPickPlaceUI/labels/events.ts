4c40cd0894508be873de642ae2f5d157e2dd7ad5
Giovanni Di Blasi <gio.diblasi@gmail.com>
2019-10-19 13:05:38 +0200

    try to use select

--- ToPickPlaceUI/labels/events.ts
+++ ToPickPlaceUI/labels/events.ts
@@ -1,6 +1,8 @@
 export const EVENTS = "Events"
+export const APP_TITLE = "Events"
 const labels = {
-    [EVENTS]: 'Events'
+    [EVENTS]: 'Events',
+    [APP_TITLE]: 'ToPickPlace'
 }
 
 export const printLabel = (labelCode: string): string => labels[labelCode] || 'NOT FOUND'; 
--- ToPickPlaceUI/pages/event/index.tsx
+++ ToPickPlaceUI/pages/event/index.tsx
@@ -1,16 +1,16 @@
 import { NextPage, NextPageContext } from 'next';
 import { AppState } from '../../store'
 import { loadEvents, selectEvent} from '../../store/events/actions';
-import { EventsActionTypes, EventsState } from '../../store/events/types';
+import { EventsActionTypes, EventsState, EventSummary } from '../../store/events/types';
 import { Dispatch } from 'redux';
 import {connect} from 'react-redux';
 import { Component } from 'react';
 import {NextPageContextWithStore} from '../../utils/nextTypes';
 import SelectionList from '../../components/SelectionList';
 import EventSummaryBox from '../../components/EventSummaryBox';
-import {EVENTS,printLabel } from '../../labels/events';
+import {EVENTS,printLabel,APP_TITLE } from '../../labels/events';
 import {styleLayout as layoutStyle, MIDDLE_COLUMN, SIDE_COLUMN} from './layoutStyle';
-import { Navbar, Alignment, Button } from '@blueprintjs/core';
+import { Navbar, Alignment, Button, Label } from '@blueprintjs/core';
 
 const mapStateToProps = (state: AppState) => ({
   counter: state.counter,
@@ -39,7 +39,9 @@ class Events extends Component<Props> {
      }
     }
   }
-
+  renderEvent = (event:EventSummary, {handleClick}) => (
+    <EventSummaryBox event={event} onSelect={handleClick}></EventSummaryBox>
+  )
   render() {
     const { events, selectEvent } = this.props;
     
@@ -47,21 +49,20 @@ class Events extends Component<Props> {
       <div>
         <Navbar>
         <Navbar.Group align={Alignment.LEFT}>
-            <Navbar.Heading>ToPickPlace</Navbar.Heading>
-            <Navbar.Divider />
+            <Navbar.Heading>{printLabel(APP_TITLE)}</Navbar.Heading>
+            <Navbar.Divider/>
+            <SelectionList
+              onSelect={selectEvent}
+              eventsSummary={events.availableEvents}
+              renderEvent={this.renderEvent}>
+                <Button text={events.selectedEvent.description || 'Select an event'} rightIcon="double-caret-vertical" />
+            </SelectionList>
             <Button className="bp3-minimal" icon="home" text="Home" />
             <Button className="bp3-minimal" icon="document" text="Files" />
         </Navbar.Group>
       </Navbar>
         <div className={SIDE_COLUMN}>
-          <SelectionList title={printLabel(EVENTS)}>
-            {events.availableEvents
-            .map(event=>
-              (<EventSummaryBox 
-                  key={event.id}
-                  event={event}
-                  onSelect={selectEvent}/>))}
-          </SelectionList>
+          
         </div>
         <div className={MIDDLE_COLUMN}>
           Selection: {events.selectedEvent.description}
