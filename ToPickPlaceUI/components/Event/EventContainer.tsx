f40ff8534e0b1af942a01b9237c83271c3ae3abb
Giovanni Di Blasi <gio.diblasi@gmail.com>
2019-11-02 13:50:18 +0100

    add layout (more or less

--- ToPickPlaceUI/components/Event/EventContainer.tsx
+++ ToPickPlaceUI/components/Event/EventContainer.tsx
@@ -2,6 +2,7 @@ import { AppState } from "../../store";
 import React, { Dispatch, Component } from "react";
 import { EventsActionTypes, Event } from "../../store/events/types";
 import {connect} from 'react-redux';
+import { SIDE_COLUMN, MIDDLE_COLUMN, styleLayout } from "../../style/layoutStyle";
 
 type Props = {
     currentEvent: Event
@@ -11,11 +12,16 @@ class EventContainer extends Component<Props>{
     render(){
         const {currentEvent} = this.props;
         return <div>
-            <h3>Current Event: {currentEvent.description}</h3>
             <div>
-                <h3>attendees</h3>
-                {currentEvent.attendees.map(attendee=>(<div>{attendee.name}</div>))}
+                <div className={MIDDLE_COLUMN}>
+                    <h3>Current Event: {currentEvent.description}</h3>
+                </div>
+                <div className={SIDE_COLUMN}>
+                    <h3>attendees</h3>
+                    {currentEvent.attendees.map(attendee=>(<div>{attendee.name}</div>))}
+                </div>
             </div>
+            <style jsx>{styleLayout}</style>
         </div>
     }
 }
