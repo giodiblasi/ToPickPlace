6673942a92d72f99d0c6a2f743940d209601c92f
Giovanni Di Blasi <gio.diblasi@gmail.com>
2019-11-16 13:47:35 +0100

    fixed dependency (#30)

--- ToPickPlaceUI/pages/_app.tsx
+++ ToPickPlaceUI/pages/_app.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import App, { AppContext } from 'next/app'
-import {rootReducer, AppState} from '../store/index'
+import {rootReducer} from '../store/index'
 import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import withRedux from 'next-redux-wrapper'
 import thunk from 'redux-thunk';
+import { AppState } from '../store/types'
 
 const getStore = (initialState: AppState) => createStore(rootReducer,initialState, composeWithDevTools(applyMiddleware(thunk)));
 
