096af00f7bd6b615601a40e10414d0e443c844fc
Giovanni Di Blasi <gio.diblasi@gmail.com>
2020-03-03 20:37:21 +0100

    WIP - configure traefik

--- ToPickPlacePlanner/Api/Startup.cs
+++ ToPickPlacePlanner/Api/Startup.cs
@@ -40,8 +40,8 @@ namespace api
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-                app.UseHttpsRedirection();
+                //app.UseHsts();
+                //app.UseHttpsRedirection();
             }
 
             SetupCulture();
