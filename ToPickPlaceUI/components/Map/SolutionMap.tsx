f6278314660c09c34e10d2e63ee5275bc7646475
Giovanni Di Blasi <gio.diblasi@gmail.com>
2020-07-28 20:39:16 +0200

    fix code

--- ToPickPlaceUI/components/Map/SolutionMap.tsx
+++ ToPickPlaceUI/components/Map/SolutionMap.tsx
@@ -3,6 +3,7 @@ import { EventMap, Solution, Attendee, Topic } from "../../store/types";
 import { mapBoardLayout, SEAT_AVAILABLE_STYLE, SEAT_BLOCKED_STYLE, SEAT_SELECTED_TOPIC } from "./mapBoardLayout";
 import { Button } from "@blueprintjs/core";
 import { printLabel, GET_SOLUTION } from "../../labels/events";
+
 type Props = {
     map: EventMap,
     solution: Solution,
@@ -10,9 +11,12 @@ type Props = {
     getSolution: () => void,
     selectedTopic?: Topic
 }
+
 type State = {
     map: EventMap
 }
+
+const AVAILABLE_SEAT = 1;
 
 export default class SolutionBoard extends Component<Props, State>{
 
@@ -24,31 +28,29 @@ export default class SolutionBoard extends Component<Props, State>{
         if(attendee && selectedTopic && attendee.topics?.includes(selectedTopic.id)){
             return SEAT_SELECTED_TOPIC;
         }
-        return value == 1 ? SEAT_AVAILABLE_STYLE : SEAT_BLOCKED_STYLE;
+        return value == AVAILABLE_SEAT ? SEAT_AVAILABLE_STYLE : SEAT_BLOCKED_STYLE;
     }
 
-    drawTable(selectedTopic?: Topic) {
+    drawTable() {
         var map = this.props.map;
         var rows = [];
-        var currentAvailableSeat = 0;
+        var currentAvailableSeat = -1;
         for (let i = 0; i < map.heigth; i++) {
             var columns = [];
             for (let c = 0; c < map.width; c++) {
+                
                 const currentIndex = (i * map.width) + c;
                 const currentValue = map.availableSeats[currentIndex];
-                const _this = this;
+                let attendee = undefined;
                 
-                const attendee = function(){
-                    if(currentValue == 1){
-                    const id = _this.props.solution.seats[currentAvailableSeat]
+                if(currentValue == AVAILABLE_SEAT){ 
                     currentAvailableSeat++;
-                        return _this.props.attendee.find(x=>x.id==id);
-                    }
-                    return undefined;
-                }();
+                    const attendeeId = this.props.solution.seats[currentAvailableSeat];
+                    attendee = this.props.attendee.find(x=>x.id==attendeeId);
+                }
                 
                 columns.push((
-                    <td className={this.getSeatStyle(currentValue, attendee, selectedTopic)}
+                    <td className={this.getSeatStyle(currentValue, attendee, this.props.selectedTopic)}
                         key={currentIndex}>
                             {attendee?.name}
                     </td>))
@@ -68,7 +70,7 @@ export default class SolutionBoard extends Component<Props, State>{
                 <div>
                     <table>
                         <tbody>
-                            {this.drawTable(this.props.selectedTopic)}
+                            {this.drawTable()}
                         </tbody>
                     </table>
                     <Button
